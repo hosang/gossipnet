4f9376ccbb3a18a541e3784340fb379656bbe7a9
Jan Hosang <hosang@users.noreply.github.com>
2017-10-02 21:22:33 +0200

    Delete setupenv.sh
    deleting system-dependent environment setup

--- setupenv.sh
+++
@@ -1,7 +0,0 @@
-# this has to be sourced
-
-
-source ~/env/py3.4-tensorflow/bin/activate
-#export LD_LIBRARY_PATH=/BS/opt/cudnn_v5.1/lib64
-export LD_LIBRARY_PATH=/BS/eldar/work/software/cudnn-8.0-v5.1/lib64:/BS/eldar/work/software/cuda-8.0/lib64
-# export CUDA_HOME=/BS/eldar/work/software/cuda-8.0
